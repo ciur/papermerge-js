ed0a513e9623f9b8e6cd2a9dba91b7a8d60e0529
Eugen Ciur <eugen@papermerge.com>
2020-07-06 10:27:18 +0200

    removed legacy components

--- src/js/side_menu/left.js
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-import {SideMenu} from "./side_menu";
-
-export class LeftMenu extends SideMenu {
-}
--- src/js/side_menu/right.js
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-import {SideMenu} from "./side_menu";
-
-export class RightMenu extends SideMenu {
-}
