f3440050d5498714685e37dbf6b0d69476d299be
Eugen Ciur <eugen@papermerge.com>
2020-11-16 10:24:50 +0100

    working on frontend widgets

--- src/js/views/widgetsbar.js
+++ src/js/views/widgetsbar.js
@@ -39,7 +39,7 @@ class SingleNodeInfoWidget extends View {
         this.node = node;
     }
 
-    render() {
+    render_to_string() {
         let context = {},
             ctype,
             _id,
@@ -63,6 +63,10 @@ class SingleNodeInfoWidget extends View {
         context['download_url'] = download_url;
 
         return this.template(context);
+    }
+
+    render() {
+        this.$el.html(this.render_to_string());
     }
 }
 
@@ -115,7 +119,7 @@ class MultiNodeInfoWidget extends View {
         downloader.download();
     }
 
-    render() {
+    render_to_string() {
         let context = {},
             message,
             count,
@@ -137,13 +141,118 @@ class MultiNodeInfoWidget extends View {
 
         return this.template(context);
     }
+
+    render() {
+        this.$el.html(this.render_to_string());
+    }
 }
 
 class MetadataWidget extends View {
 
+    /**
+        This must be set to widgetsbar element.
+
+        For sake of events delegation this element must exist 
+        BEFORE metadata widget is rendered.
+    **/
+    el() {
+        // sidebar container of all widgets
+        return $("#widgetsbar");
+    }
+
+    widget_el() {
+        // DOM element containing all metadata
+        return $(".metadata-widget");
+    }
+
     initialize(node) {
         this.node = node;
         this.metadata = new Metadata(node);
+    }
+
+    events() {
+        let event_map = {
+          "click .add-metadata-key": "_add_metadata_key",
+          "click .close.key": "_remove_metadata_key",
+          "keyup input": "_update_value",
+          "change input": "_update_value",
+          "change .kv_type": "_kv_type_update",
+          "change .kv_format": "_kv_format_update",
+          "click button.save": "_on_save"
+        }
+
+        return event_map;
+    }
+
+
+    _kv_format_update(event) {
+
+        let value = $(event.currentTarget).val();
+        let parent = $(event.currentTarget).closest("li");
+        let data = parent.data();
+
+        this.metadata.update_simple(data['cid'],'kv_format', value);
+
+        this.render();  
+    }
+
+    _kv_type_update(event) {
+
+        let value = $(event.currentTarget).val();
+        let parent = $(event.currentTarget).closest("li");
+        let data = parent.data();
+        let cur_fmt = {};
+
+        cur_fmt['money'] = this.metadata.get('currency_formats');
+        cur_fmt['numeric'] = this.metadata.get('numeric_formats');
+        cur_fmt['date'] = this.metadata.get('date_formats');
+        cur_fmt['text'] = [];
+
+        this.metadata.update_simple(data['cid'],'kv_type', value);
+        this.metadata.update_simple(data['cid'],'current_formats', cur_fmt[value]);
+        if (cur_fmt[value].length > 0) {
+            // kv_format entry is a 2 items array. First one is used as value
+            // in HTML <option> and second one is the human text
+            // cur_fmt[value][0][0] == use first *value* of first format from the list
+            this.metadata.update_simple(data['cid'],'kv_format', cur_fmt[value][0][0]);
+        } else {
+            // current list of formatting types is empty only for kv_type text
+            // no formating - means kv_type = text
+            this.metadata.update_simple(data['cid'],'kv_format', "");
+        }
+
+        this.render();
+    }
+
+    _update_value(event) {
+        let value = $(event.currentTarget).val();
+        let parent = $(event.currentTarget).closest("li");
+        let data = parent.data();
+
+        this.metadata.update_simple(data['cid'], 'key', value);
+    }
+
+    _add_metadata_key(event) {
+        let value = $(event.currentTarget).val();
+        
+        this.metadata.add_simple();
+        
+        this.render();
+    }
+
+    _remove_metadata_key(event) {
+        let parent = $(event.currentTarget).parent();
+        let $li_container = $(event.currentTarget).closest("li");
+        let data = parent.data();
+
+        this.metadata.remove_simple(data['cid']);
+        
+        $li_container.remove();
+        this.render();
+    }
+
+    _on_save() {
+        this.metadata.save();
     }
 
     template(kwargs) {
@@ -159,7 +268,7 @@ class MetadataWidget extends View {
         return this.metadata.all_disabled;
     }
 
-    render() {
+    render_to_string() {
         let context;
 
         context = {
@@ -169,6 +278,14 @@ class MetadataWidget extends View {
         }
 
         return this.template(context);
+    }
+
+    render() {
+        // notice that widget will be rendered in
+        // this.widget_el()
+        this.widget_el().html(
+            this.render_to_string()
+        );
     }
 }
 
@@ -228,8 +345,8 @@ export class WidgetsBarView extends View {
             this.info_widget = new SingleNodeInfoWidget(node);
             this.metadata_widget = new MetadataWidget(node);
 
-            compiled += this.info_widget.render();
-            compiled += this.metadata_widget.render();
+            compiled += this.info_widget.render_to_string();
+            compiled += this.metadata_widget.render_to_string();
 
             if (parts) {
                 for (i=0; i < parts.length; i++) {
@@ -250,7 +367,7 @@ export class WidgetsBarView extends View {
                 this.metadata_widget = undefined;
             }
             this.info_widget = new MultiNodeInfoWidget(selection);
-            compiled += this.info_widget.render();
+            compiled += this.info_widget.render_to_string();
         }
 
 
